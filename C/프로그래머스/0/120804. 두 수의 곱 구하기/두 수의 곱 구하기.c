3590e9cd24581cc7960a46987aa54e77f8bb78c7
Bathboomsoso <167727156+Bathboomsoso@users.noreply.github.com>
2025-04-12 19:58:39 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.01 ms, Memory: 4.59 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if(-1<num1<101){
+        if(-1<num2<101){
+            answer = num1 * num2;
+            return answer;
+        }
+    }
+        
+    
+}
