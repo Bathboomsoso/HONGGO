6c7e1fdcb83fe2dfb97350ba78220f738c9e0e14
Bathboomsoso <167727156+Bathboomsoso@users.noreply.github.com>
2025-05-27 19:29:07 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- C++/프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ C++/프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <numeric>
 
+using namespace std;
+
 double solution(const std::vector<int> arr) {
     return std::accumulate(arr.begin(), arr.end(), 0.0) / arr.size();
 }
